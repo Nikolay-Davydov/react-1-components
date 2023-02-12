9b0fccd57b6a6e2bc38214b3b8f4bda228bd6afe
Nikolay-Davydov <davidovns70@gmail.com>
2023-02-12 17:18:19 +0100

    init

---
+++ components1.1/src/ShopItemFunc.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function ShopItemFunc(props) {
+    return (
+      <div>
+        <div class="main-content">
+    <h2>{props.item.brand}</h2>
+    <h1>{props.item.title}</h1>
+    <h3>{props.item.description}</h3>
+    <div class="description">
+      {props.item.descriptionFull}
+    </div>
+    <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+    <div class="divider"></div>
+    <div class="purchase-info">
+      <div class="price">{props.item.currency}{props.item.price.toFixed(2)}</div>
+      <button>Добавить в корзину</button>
+    </div>
+  </div>
+      </div>
+    );
+  }
